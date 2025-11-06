802b54f16d5ca32a87b7116aa71d8d71d6f1876a
Florecita Wenny <h1101241039@student.untan.ac.id>
2025-11-06 14:24:59 +0700

    ypp

--- config/database.php
+++ config/database.php
@@ -1,10 +1,9 @@
 <?php
-// Railway menyediakan MySQL variables otomatis
 return [
-    'host' => getenv('DB_HOST') ?: '127.0.0.1',
-    'port' => getenv('DB_PORT') ?: '3306',
-    'dbname' => getenv('DB_NAME') ?: 'mvc_db',
-    'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: 'mamakdie-00',
+    'host' => getenv('MYSQLHOST') ?: '127.0.0.1',
+    'port' => getenv('MYSQLPORT') ?: '3306',
+    'dbname' => getenv('MYSQLDATABASE') ?: 'mvc_db',
+    'username' => getenv('MYSQLUSER') ?: 'root',
+    'password' => getenv('MYSQLPASSWORD') ?: '',
     'charset' => 'utf8mb4'
 ];
