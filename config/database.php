068a79448b43091184593bed45b1b6a713f3a16d
Florecita Wenny <h1101241039@student.untan.ac.id>
2025-11-07 11:10:49 +0700

    convig/adtabase

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'host' => getenv('DB_HOST') ?: '127.0.0.1',
-    'port' => getenv('DB_PORT') ?: '3306',
-    'dbname' => getenv('DB_NAME') ?: 'mvc_db',
-    'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: 'mamakdie-00',
+    'host' => getenv('MYSQLHOST') ?: 'containers-us-west-54.railway.app',
+    'port' => getenv('MYSQLPORT') ?: '3306',
+    'dbname' => getenv('MYSQLDATABASE') ?: 'railway',
+    'username' => getenv('MYSQLUSER') ?: 'root',
+    'password' => getenv('MYSQLPASSWORD') ?: '2GyzxM9yDsgvLp2ff4Qk',
     'charset' => 'utf8mb4'
 ];
